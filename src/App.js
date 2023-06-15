da625d40130d09127628458bdc2ccff836de1a29
Nitin1604 <nitinsokhal@gmail.com>
2023-06-15 23:23:18 +0530

    forwardRef in React JS

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ function App() {
   let inputRef=useRef(null)
   function updateInput()
   {
-    inputRef.current.value="1000";
+    inputRef.current.value="react js series";
     inputRef.current.style.color="red"
     inputRef.current.focus()
-
+   
 
   }
   return (
     <div className="App">
       <h1>forwardRef in React </h1>
       <User ref={inputRef} />
-      <button onClick={updateInput} >Update Input Box</button>
+      <button  type="button" className="btn btn-danger my-3" onClick={updateInput} >Update Input Box</button>
     </div>
   );
 
