0f05a7306b19cfad764d3fe2e3298c5feddfce2d
Nitin1604 <nitinsokhal@gmail.com>
2023-06-15 23:08:45 +0530

    App.js code

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
 import './App.css';
-import React from 'react'
-class App extends React.Component {
-  constructor()
+import React ,{useRef} from 'react'
+import User from './User'
+function App() {
+  let inputRef=useRef(null)
+  function updateInput()
   {
-    super();
-    console.warn("constructor")
-    this.state={
-      count:0
-    }
-  }
-  componentDidUpdate(preProps , preState , snapshot)
-  {
-    // console.warn("componentDidUpdate", preState.count, this.state.count)
-    console.warn("componentDidUpdate",snapshot)
-    if (preState.count === this.state.count)
-    {
-      console.log("Data is already same")
-    }
+    inputRef.current.value="1000";
+    inputRef.current.style.color="red"
+    inputRef.current.focus()
+
 
   }
-  render()
-  {
-    console.warn("render")
+  return (
+    <div className="App">
+      <h1>forwardRef in React </h1>
+      <User ref={inputRef} />
+      <button onClick={updateInput} >Update Input Box</button>
+    </div>
+  );
 
-    return (
-      <div className="App">
-        <h1>Component Did Update {this.state.count}</h1>
-        <button  type="button" className="btn btn-danger" onClick={()=>{this.setState({count: this.state.count })}}>Update</button>
-      </div>
-    );
-  }
 }
-
 export default App;
