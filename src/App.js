1dc180bfd65f10627b4f20838d2d428181ac6c5f
Nitin1604 <nitinsokhal@gmail.com>
2023-06-16 18:21:57 +0530

    shouldComponentUpdate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import './App.css';
-import React ,{useRef} from 'react'
-import User from './User'
-function App() {
-  let inputRef=useRef(null)
-  function updateInput()
+import React from 'react'
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state={
+      count:0
+    }
+  }
+  shouldComponentUpdate()
   {
-    inputRef.current.value="react js series";
-    inputRef.current.style.color="red"
-    inputRef.current.focus()
-   
+    console.warn("shouldComponentUpdate",this.state.count)
+   if(this.state.count<5)
+   {
+     return true;
+   }
 
   }
-  return (
-    <div className="App">
-      <h1>forwardRef in React </h1>
-      <User ref={inputRef} />
-      <button  type="button" className="btn btn-danger my-3" onClick={updateInput} >Update Input Box</button>
-    </div>
-  );
 
+  render() {
+    return (
+      <div className="App">
+        <h1> Should Component Update  {this.state.count}</h1>
+        <button type="button" className="btn btn-danger my-3" onClick={()=>this.setState({count:this.state.count+1})}>Update Counter</button>
+      </div>
+    );
+  }
 }
+
 export default App;
